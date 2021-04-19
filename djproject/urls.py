9523cc2fe9aad8e6fd5baac9995d2eae49f3361c
Abdul Halim <abdul.halim@bs-23.net>
2021-04-20 03:56:20 +0600

    [IMP] Implemented ListView to home view
    function based home view change to class based ListView.
    Url updated
    test case also updated according to this.

--- djproject/urls.py
+++ djproject/urls.py
@@ -20,7 +20,7 @@ from boards import views
 from accounts import views as account_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.BoardListView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('signup/', account_views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),

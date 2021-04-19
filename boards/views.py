21f60e49312446a6f2a0b8ab1869b5e226cf07fb
Abdul Halim <abdul.halim@bs-23.net>
2021-04-20 03:32:56 +0600

    [IMP] edit post: login required, and get_queryset implemented
    now login required for edit post.
    get_queryset() implemented to filter post by the logged in user

--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,11 @@
+from django.utils import timezone
+from django.utils.decorators import method_decorator
 from django.db.models import Count
 from django.views.generic import UpdateView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
-from django.utils import timezone
 
 
 def home(request):
@@ -62,12 +63,17 @@ def reply_topic(request, pk, topic_pk):
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
 
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message',)
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super(PostUpdateView, self).get_queryset()
+        return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
         post = form.save(commit=False)

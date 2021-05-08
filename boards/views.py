d798dfb7c6c0a7105ca9913034bb0702000cb4a5
Abdul Halim <abdulhalim.cu10@gmail.com>
2021-05-08 23:26:11 +0600

    final changes

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.db.models import Count
 from django.views.generic import UpdateView, ListView
@@ -87,7 +88,14 @@ def reply_topic(request, pk, topic_pk):
             topic.last_updated = timezone.now()
             topic.save()
 
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
